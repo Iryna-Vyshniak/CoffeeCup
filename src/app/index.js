556bad2fa083af580dd1e6892bb331512e8a549c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-03-08 22:51:25 +0200

    Add click event listener to the menu. Toggle the active and active state of the menu.

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+// Sets the nav and menuBtn.
+const nav = document.querySelector('.nav');
+const menuBtn = document.querySelector('.header__menu-btn');
+// Add click event listener to the menu. Toggle the active and active state of the menu.
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  nav.classList.toggle('active');
+});
