cd0703c59de0b5ff460029235f6079304f5be735
Iryna-Vyshniak <111734415+Iryna-Vyshniak@users.noreply.github.com>
2023-03-09 18:26:45 +0000

    Deploying to gh-pages from @ Iryna-Vyshniak/CoffeeCup@526f014bcb49a5368b464fa263dd5bbbd0fd2906 🚀

--- index.6cf971b5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".nav"),t=document.querySelector(".header__menu-btn");t.addEventListener("click",(function(){t.classList.toggle("active"),e.classList.toggle("active")}))}();
-//# sourceMappingURL=index.6cf971b5.js.map
